9f53b20627ee79d27b506eebe70a8df689205a54
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-20 19:59:40 -0400

    continuando en la construccion del template html y su estructura de loginn

--- login/index.php
+++ login/index.php
@@ -14,6 +14,26 @@
  <head>
  	<meta charset="UTF-8">
  	<title>Login | smartTecApp</title>
+
+    <!-- viewport -->
+     	<meta name="viewport" content="width=device-width,
+     	            user-scalable=no,
+     	            initial-scale=1.0,
+     	            maximum-scale=1.0,
+     	            minimum-scale=1.0">
+
+    <!-- styles css  -->
+
+    <!-- own library  -->
+    <link rel="stylesheet" href="../public/css/lib/jeremias-lib/jeremias-lib.css" />
+
+    <!-- login -->
+    <link rel="stylesheet" href="../public/css/login.css" />
+
+    <!-- icon -->
+    <link rel="stylesheet" href="../public/css/login.css" />
+
+
  </head>
  <body>
 
