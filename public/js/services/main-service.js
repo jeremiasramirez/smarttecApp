df20ab6e5fec5fb897ce3c0bcd6f128bb5bbcf63
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-20 23:07:29 -0400

    agregando servicios en angularjs

---
+++ public/js/services/main-service.js
@@ -0,0 +1,14 @@
+/*
+* main service
+* */
+var smartTecApp = angular.module("service.main", []);
+
+
+smartTecApp.factory("dataRequest", [function($http){
+
+
+    var data = {};
+
+    return data;
+
+}]);
