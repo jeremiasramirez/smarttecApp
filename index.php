26d87f1e272ba3980c8de81782500bcc3c56e0ae
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-20 20:33:30 -0400

    reutilizando estructura de login al archivo principal

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+    /*
+        ~   future code   ~
+    */
+
+?>
+
+<!DOCTYPE html>
+ <html lang="es">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Login | smartTecApp</title>
+
+    <!-- viewport -->
+     	<meta name="viewport" content="width=device-width,
+     	            user-scalable=no,
+     	            initial-scale=1.0,
+     	            maximum-scale=1.0,
+     	            minimum-scale=1.0">
+
+    <!--    styles css  -->
+
+    <!--    own library -->
+    <link rel="stylesheet" href="public/css/lib/jeremias-lib/jeremias-lib.css" />
+
+    <!--    main css   -->
+    <link rel="stylesheet" href="public/css/main.css" />
+
+    <!--    icon   -->
+    <link rel="stylesheet" href="public/css/lib/fontawesome/css/all.css" />
+
+ </head>
+ <body>
+
+
+
+
+
+
+
+
+    <!--  *****  script js  ***** -->
+
+    <!--    easy dom    -->
+    <script src="public/js/lib/easydom/easydom.js">  </script>
+
+ </body>
+ </html>
